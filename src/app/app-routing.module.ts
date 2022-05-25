2a4b77ab6b64932ba692e7b38f2745fd56883015
Steve Schaner <schanerst@gmail.com>
2022-05-25 12:23:07 -0400

    Bootstrap and jQuery installed, along with Navigation and footer started.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './home/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
